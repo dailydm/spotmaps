f6f30bc39d6fc6f6545b44c06d9e00bf349e32e3
David <dailydm1227@gmail.com>
2018-08-25 11:23:01 -0600

    refactor

--- App.js
+++ App.js
@@ -1,48 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { MapView } from 'expo';
-import { getCurrentLocation } from './App/Services/LocationService';
+import SpotLocator from './App/Components/SpotLocator';
 
 export default class App extends React.Component {
 
-  constructor(props){
-    super(props);
-    let _mapView;
-    this.state = {
-      location: null,
-    }
-  }
-
-  componentDidMount(){
-    getCurrentLocation().then(location => {
-      this._mapView.animateToCoordinate({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      })
-      this.setState({ location });
-    });
-  }
-
   render() {
     return (
-      <MapView
-        style={{ flex: 1 }}
-        ref={ mapView => this._mapView = mapView}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        { this.state.location &&
-          <MapView.Marker coordinate={{
-             latitude: this.state.location.coords.latitude,
-             longitude: this.state.location.coords.longitude,
-            }}
-          />
-        }
-      </MapView>
+      <SpotLocator />
     );
   }
 }
---
+++ App/Components/SpotLocator.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { MapView } from 'expo';
+import PropTypes from 'prop-types';
+import { getCurrentLocation } from '../Services/LocationService';
+
+export default class SpotLocator extends React.Component {
+
+  constructor(props){
+    super(props);
+    let _mapView;
+    this.state = {
+      initialRegion: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      currentRegion: null
+    };
+  }
+
+  componentDidMount(){
+    getCurrentLocation().then(location => {
+      console.log(JSON.stringify(location));
+      this.setState({
+        currentRegion: {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }
+      });
+      this._mapView.animateToRegion(this.state.currentRegion, 3000);
+    })
+  }
+
+  render(){
+    const { initialRegion } = this.state
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        ref={ mapView => this._mapView = mapView}
+        initialRegion={ initialRegion }
+      />
+    );
+  }
+}
