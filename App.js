b457215445ac0ef10eae1046e9f4a9439aa626ff
David <dailydm1227@gmail.com>
2018-08-31 16:31:10 -0600

    add non functioning plus button to map

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Container } from 'native-base';
 import SpotLocator from './App/Components/SpotLocator';
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <SpotLocator />
+      <Container>
+        <SpotLocator />
+      </Container>
     );
   }
 }
--- App/Components/SpotLocator.js
+++ App/Components/SpotLocator.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { MapView } from 'expo';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import PropTypes from 'prop-types';
+import { MapView } from 'expo';
+import { Button, Icon } from 'native-base';
+
 import { startLocationTracking } from '../Services/LocationService';
+
 
 export default class SpotLocator extends React.Component {
 
@@ -39,12 +43,38 @@ export default class SpotLocator extends React.Component {
   render(){
     const { initialRegion } = this.state
     return (
-      <MapView
-        style={{ flex: 1 }}
-        ref={ mapView => this._mapView = mapView}
-        initialRegion={ initialRegion }
-        showsUserLocation={true}
-      />
+      <View style={{flex: 1}}>
+        <MapView
+          provider="google"
+          showsUserLocation={true}
+          style={styles.map}
+          ref={ mapView => this._mapView = mapView}
+          initialRegion={ initialRegion }
+        />
+        <TouchableOpacity
+           style={{
+               borderWidth:1,
+               borderColor:'rgba(0,0,0,0.2)',
+               alignItems:'center',
+               justifyContent:'center',
+               width:50,
+               position: 'absolute',
+               bottom: 25,
+               right: 15,
+               height:50,
+               backgroundColor:'#3f4afe',
+               borderRadius:100,
+             }}
+         >
+         <Icon name="add"  size={20} iconStyle={{color: "white"}} />
+       </TouchableOpacity>
+     </View>
     );
   }
 }
+
+styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  }
+})
